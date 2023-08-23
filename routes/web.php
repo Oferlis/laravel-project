51066e563b71b642d5a0c16e780c95767429b326
Ofer Lis <oferlis1@gmail.com>
2023-08-23 14:26:09 +0300

    added login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,7 +16,34 @@ use App\Models\Listing;
 
 Route::get('/', [ListingController::class, "index"]);
 
-//Show Create Form
+// Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, "edit"]);
+
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, "update"]);
+
+// Delete Listing
+Route::delete('/listings/{listing}', [ListingController::class, "destroy"]);
+
 Route::get('/listings/{listing}', [ListingController::class, "show"]);
+
+// Show Register Create Form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout']);
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login']);
+
+// Login User
+Route::post('/users/authenticate', [UserController::class, "authenticate"]);
